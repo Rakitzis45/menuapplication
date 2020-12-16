8bd784d877b1ee6f2559c148de22dc09c21eea5d
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-16 12:45:45 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :items
+  resources :categories
+  resources :cuisines
+  resources :reviews
+  resources :restaurants
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

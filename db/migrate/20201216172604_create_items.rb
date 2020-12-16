7e3404c26dda9da17845cf563917bbbdd429d517
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-16 12:47:22 -0500

    Create 20201216172604_create_items.rb

---
+++ db/migrate/20201216172604_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.float :price
+      t.integer :category_id
+      
+      t.timestamps
+    end
+  end
+end

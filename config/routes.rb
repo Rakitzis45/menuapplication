db4c4b74420f633365f11a779a4349ca6036b217
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-16 12:53:43 -0500

    added sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :restaurants
   resources :users
+
+  resource :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

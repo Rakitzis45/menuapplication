a0906fac39385b34b7a1557312925317a8bd6fa6
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-18 15:07:21 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,18 @@ class User < ApplicationRecord
 
     has_many :reviews
     has_many :restaurants
+
+    validates_uniqueness_of :email
+    validates_presence_of :name, :email, :password,
+    validates_uniqueness_of :email
+    validates :password, length: { in: 6..20 }
+    
+
+    def self.find_or_create_from_omniauth(access_token)
+        User.find_or_create_by(email: access_token.info.email) do |u|
+            u.name = access_token.info.first_name
+            u.password = SecureRandom.hex(12)
+        end
+    end
     
 end

be3fa37157c8cd5edbc66c51436794dee54a7750
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-18 15:08:03 -0500

    Update restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,47 @@
 class RestaurantsController < ApplicationController
+    def index
+        search
+    end
+
+    def show
+    end
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create
+        @restaurant = Restaurant.create(restaurant_params) do |f|
+            f.user_id = session[:user_id]
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+    private
+    def search
+        if params[:search].empty?
+            redirect_to search_path
+        end
+        @restaurants = Restaurant.where(state: params[:search])
+        if @restaurants.empty?
+            @restaurants = Restaurant.where(zipcode: params[:search])
+        end
+        if @restaurants.empty?
+            @restaurants = Restaurant.where(cuisine: params[:search])
+        end
+    end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :phone_number, :address, :state, :zipcode, cuisine_ids: [] )
+    end
+
 end

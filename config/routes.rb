fecbd3ca0ca8fcd9557cae02630114f7a047d04e
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-21 18:29:48 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   
   resources :items
-  resources :categories
+  resources :categories do 
+    resources :items 
+  end
   resources :cuisines
   resources :reviews
-  resources :restaurants
+  resources :restaurants do 
+    get '/menu', to: 'restaurants#menu'
+    resource :categories
+  end
   resources :users do
     resources :restaurants
+    get '/myrestaurants', to:'users#myrestaurants'
   end
+
+  
 
   get '/search', to: 'static#search'
   root "static#search"
@@ -18,6 +26,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#googleAuth'
 
   get '/logout', to: 'sessions#destroy'
+
 
 
 end

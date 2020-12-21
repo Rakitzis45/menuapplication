3ccae6bb761144b1b5d698aed91439521c75c057
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-21 18:24:08 -0500

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,35 @@
 class CategoriesController < ApplicationController
+    def new
+        @category = Category.new
+        
+    end
+
+    def create
+        @category = Category.new(categories_params)
+        @restaurant = Restaurant.find_by(id:categories_params[:restaurant_id])
+        if @category.save
+            redirect_to restaurant_menu_path(@restaurant)
+        else
+            render 'catergories/new'
+        end
+    end
+
+    def edit
+        @category = Category.find_by(id:params[:format])
+    end
+
+    def update
+        @category = Category.find_by(id:params[:id])
+        @restaurant = Restaurant.find_by(id:categories_params[:restaurant_id])
+        if @category.update(categories_params)
+            redirect_to restaurant_menu_path(@restaurant)
+        else
+            render 'categories/edit'
+        end
+    end
+
+    private
+    def categories_params
+        params.require(:category).permit(:name, :restaurant_id)
+    end
 end

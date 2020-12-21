15cded144e1562b3549c14b21465c63050c659e2
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-21 18:24:20 -0500

    Update restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,16 @@ class RestaurantsController < ApplicationController
         search
     end
 
+    def menu
+        find_user
+        @restaurant = Restaurant.find_by(id:params[:restaurant_id])
+    end
+
     def show
+        find_restaurant
+        find_user
+        @review = Review.new
+        @reviews = @restaurant.reviews.all
     end
 
     def new
@@ -17,9 +26,23 @@ class RestaurantsController < ApplicationController
     end
 
     def edit
+        find_user
+        find_restaurant
+        
+        if @user == nil || @user.id != @restaurant.user.id
+            redirect_to restaurant_path(@restaurant)
+        else  @user.id == @restaurant.user.id
+            render "restaurants/edit"
+        end
     end
 
     def update
+        find_restaurant
+        if @restaurant.update(restaurant_params)
+            redirect_to restaurant_path(@restaurant)
+        else
+            render '"restaurants/edit"'
+        end
     end
 
     def destroy
@@ -41,7 +64,16 @@ class RestaurantsController < ApplicationController
     end
 
     def restaurant_params
-        params.require(:restaurant).permit(:name, :phone_number, :address, :state, :zipcode, cuisine_ids: [] )
+        params.require(:restaurant).permit(:name, :phone_number, :address, :city, :state, :zipcode, cuisine_ids: [] )
     end
+
+    def find_restaurant
+        @restaurant = Restaurant.find_by(id:params[:id])
+    end
+
+    def find_user 
+        @user = User.find_by(id:session[:user_id])
+    end
+
 
 end

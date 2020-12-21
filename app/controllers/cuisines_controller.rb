8e8f6a5577415b6e90330892aa118cfc0744a24d
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-21 18:59:29 -0500

    Update cuisines_controller.rb

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,4 +1,4 @@
 class CuisinesController < ApplicationController
     has_many :restaurant_cuisines
-    has_many :restaurants, through: :restaurant
+    has_many :restaurants, through: :restaurant_cuisines
 end

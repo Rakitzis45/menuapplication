04c4b6b014aff27ee49881db9f6d284c6be4cad7
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-21 18:23:14 -0500

    added find_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
     def current_user
         session[:user_id]
     end
+
+    def find_user 
+        @user = User.find_by(id:current_user)
+    end
 end

6bf274bd651f0fdce649ab1f955ad877f9291488
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-21 18:24:24 -0500

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+    
+    def create
+        @review = Review.new(review_params)
+        @review.user_id = current_user
+        if @review.save
+            redirect_to restaurant_path(@review.restaurant)
+        else
+            render 'restaurants/show'
+        end
+    
+    end
+
+    def destroy
+        review = Review.find_by(id:params[:id])
+        restaurant_id = review.restaurant.id
+        review.destroy
+        redirect_to restaurant_path(restaurant_id)
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:name, :post, :food_rating, :service_rating, :user_id, :restaurant_id)
+    end
 end

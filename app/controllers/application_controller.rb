849104aeac84cd875bad990ecdc303041f3ca15b
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-23 14:42:19 -0500

    added require_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
     private
     def current_user
         session[:user_id]
@@ -6,5 +7,16 @@ class ApplicationController < ActionController::Base
 
     def find_user 
         @user = User.find_by(id:current_user)
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def require_login
+        unless logged_in?
+            flash[:error] = "You must be logged in to access this section"
+            redirect_to login_path
+        end
     end
 end

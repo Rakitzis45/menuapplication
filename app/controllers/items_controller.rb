4dbeacecd1632a4c820d7767f466f1d8d4e4d010
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-23 14:43:23 -0500

    Update items_controller.rb
    added destroy route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,15 @@
 class ItemsController < ApplicationController
     def new
         @item = Item.new
+        @category = Category.find_by(id:params[:category_id])
     end
 
     def create
         
         @item = Item.new(items_params)
-        category = Category.find_by(id:items_params[:category_id])
+        @category = Category.find_by(id:items_params[:category_id])
         if @item.save
-            redirect_to restaurant_menu_path(category.restaurant)
+            redirect_to restaurant_menu_path(@category.restaurant)
         else
             render 'items/new'
         end
@@ -16,15 +17,24 @@ class ItemsController < ApplicationController
 
     def edit
         find_item
+        @category = Category.find_by(id:params[:category_id])
     end
 
     def update
         find_item
+        @category = Category.find_by(id:items_params[:category_id])
         if @item.update(items_params)
             redirect_to restaurant_menu_path(@item.category.restaurant)
         else
             render 'items/edit'
         end
+    end
+
+    def destroy
+        find_item
+        @restaurant = @item.category.restaurant
+        @item.destroy
+        redirect_to restaurant_menu_path(@restaurant)
     end
 
     private

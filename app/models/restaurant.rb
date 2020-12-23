495fdbcc615242336a426650cb7d8c04f496a403
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-23 14:50:33 -0500

    added validations to restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,11 @@ class Restaurant < ApplicationRecord
     belongs_to :user
 
     accepts_nested_attributes_for :restaurantcuisines
+
+    validates_presence_of :name, :phone_number, :address, :city, :state, :zipcode, :cuisine_ids, :user_id
+    validates_length_of :phone_number, is: 10
+    validates_length_of :zipcode, is: 5
+    validates_length_of :state, is: 2
+
+    scope :search_zipcode, -> (zipcode) {where("zipcode = ?", zipcode)}
 end

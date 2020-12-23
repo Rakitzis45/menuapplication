6c6c75628ac01ab85ae9c0eb19f19778558464fb
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-23 14:50:14 -0500

    added validations to review class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
+
+    validates_presence_of :user_id, :restaurant_id, :service_rating, :food_rating, :post
+    validates_inclusion_of :service_rating, :food_rating, :in => 0..5
 end

229d6f7f074cd0c23f483a5f8885cbb58970e1c2
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-12-23 14:42:44 -0500

    Update categories_controller.rb
    added destroy route and before_action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,27 @@
 class CategoriesController < ApplicationController
+    before_action :require_login, only: [:new, :create, :edit, :update]
+
     def new
         @category = Category.new
-        
+        @restaurant = Restaurant.find_by(id:params[:restaurant_id])
+        session[:last_restaurant] = @restaurant.id
     end
 
     def create
-        @category = Category.new(categories_params)
-        @restaurant = Restaurant.find_by(id:categories_params[:restaurant_id])
+        @category = Category.new(categories_params) do |f|
+            f.restaurant_id = session[:last_restaurant]
+        end
+        @restaurant = Restaurant.find_by(id:session[:last_restaurant])
         if @category.save
             redirect_to restaurant_menu_path(@restaurant)
         else
-            render 'catergories/new'
+            render 'categories/new'
         end
     end
 
     def edit
         @category = Category.find_by(id:params[:format])
+        @restaurant = Restaurant.find_by(id:params[:restaurant_id])
     end
 
     def update
@@ -28,8 +34,19 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def destroy
+        find_category
+        @restaurant = @category.restaurant
+        @category.destroy
+        redirect_to restaurant_menu_path(@restaurant)
+    end
+
     private
     def categories_params
         params.require(:category).permit(:name, :restaurant_id)
+    end
+
+    def find_category
+        @category = Category.find_by(id:params[:id])
     end
 end
